5ed19d8eebea1bdd4cad7e0998be1d91dab108b1
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-07-11 20:52:23 +0300

    Add three components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,16 @@
 import WelcomeMessage from "./WelcomeMessage";
+import WelcomeMessage from "./Header";
+import WelcomeMessage from "./MainCotent";
+import WelcomeMessage from "./Footer";
 import "./App.css";
 
 function App() {
   return (
     <>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   );
 }
