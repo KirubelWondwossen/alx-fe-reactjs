20c4967e2ca5592bc961c9ac92818c09c58211e0
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-07-12 20:34:06 +0300

    Fix the bug

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
-import { Header } from "./Header";
-import { MainContent } from "./MainContent";
-import { Footer } from "./Footer";
+import Header from './Header'; // prettier-ignore
+import MainContent from './MainContent'; // prettier-ignore
+import Footer from './Footer'; // prettier-ignore
+
 import "./App.css";
 
 function App() {
