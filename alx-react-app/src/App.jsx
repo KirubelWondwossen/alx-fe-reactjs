1fccc25c9d7e409d1728e55cea3030e076f6e380
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-07-12 20:42:48 +0300

    Add props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './Header'; // prettier-ignore
 import MainContent from './MainContent'; // prettier-ignore
 import Footer from './Footer'; // prettier-ignore
+import UserProfile from './UserProfile'; // prettier-ignore
 
 import "./App.css";
 
 function App() {
   return (
     <>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Header />
       <MainContent />
       <Footer />
