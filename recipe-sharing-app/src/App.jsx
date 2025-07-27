6eae9b8cc2d58887b3b5c0fff6cd64f7c2dd6993
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-07-27 20:53:08 +0300

    Add edit and delete functionality

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <RecipeList />
-      <AddRecipeForm />
-    </>
+    <Router>
+      <Routes>
+        {/* Home Page: List & Add Recipe Form */}
+        <Route
+          path="/"
+          element={
+            <>
+              <RecipeList />
+              <AddRecipeForm />
+            </>
+          }
+        />
+
+        {/* Recipe Details Page (with Edit & Delete) */}
+        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+      </Routes>
+    </Router>
   );
 }
+
+// Wrapper for extracting `id` param
+import { useParams } from "react-router-dom";
+const RecipeDetailsWrapper = () => {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={parseInt(id)} />;
+};
 
 export default App;
