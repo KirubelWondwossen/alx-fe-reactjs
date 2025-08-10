2e28b8930b19a5e670f1a0b74ec9ee514d9daeb7
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-08-10 21:40:14 +0300

    Add the env filr

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,36 +1,62 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
-import "./App.css";
+import FavoritesList from "./components/FavoritesList";
+import RecommendationsList from "./components/RecommendationsList";
+import useRecipeStore from "./components/recipeStore";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* Home Page: List & Add Recipe Form */}
-        <Route
-          path="/"
-          element={
-            <>
-              <RecipeList />
-              <AddRecipeForm />
-            </>
-          }
-        />
-
-        {/* Recipe Details Page (with Edit & Delete) */}
-        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
-      </Routes>
-    </Router>
-  );
+function RecipeDetailsWrapper({ id }) {
+  return <RecipeDetails recipeId={parseInt(id)} />;
 }
 
-// Wrapper for extracting `id` param
 import { useParams } from "react-router-dom";
-const RecipeDetailsWrapper = () => {
+const RecipeDetailsRoute = () => {
   const { id } = useParams();
   return <RecipeDetails recipeId={parseInt(id)} />;
 };
+
+function App() {
+  const generateRecommendations = useRecipeStore(
+    (state) => state.generateRecommendations
+  );
+  const favorites = useRecipeStore((state) => state.favorites);
+
+  // Regenerate recommendations whenever favorites change
+  useEffect(() => {
+    generateRecommendations();
+  }, [favorites, generateRecommendations]);
+
+  return (
+    <Router>
+      <div style={{ display: "flex", gap: "2rem" }}>
+        {/* Left Column: Recipe List + Add Recipe */}
+        <div style={{ flex: 3 }}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <RecipeList />
+                  <AddRecipeForm />
+                </>
+              }
+            />
+            <Route path="/recipe/:id" element={<RecipeDetailsRoute />} />
+          </Routes>
+        </div>
+
+        {/* Right Column: Favorites and Recommendations */}
+        <div style={{ flex: 1 }}>
+          <FavoritesList />
+          <hr />
+          <RecommendationsList />
+        </div>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
